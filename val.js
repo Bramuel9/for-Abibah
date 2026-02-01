98106c14546043be833f4a35e7aa80e0946a53e7
Bramuel9 <bramwelwanjala77@gmail.com>
2026-02-01 12:39:44 +0300

    Add files via upload

---
+++ val.js
@@ -0,0 +1,40 @@
+const clickMeBtn = document.getElementById('click-me');
+const music = document.getElementById('bgMusic');
+const toggleBtn = document.querySelectorAll('button')[1];
+
+
+clickMeBtn.addEventListener('click', showLove);
+
+const herName = prompt("What's your name? 💖");
+
+if (herName) {
+    document.getElementById('title').innerHTML = `Hey ${herName} 😌❤️`;
+    document.getElementById('introText').innerHTML =
+        `Hey ${herName}, I made this just for you…  
+        because you make my days brighter and my smile easier 😏`;
+}
+
+function showLove() {
+    document.getElementById('hiddenText').style.display = 'block';
+    document.getElementById('bgMusic').play();
+}
+
+for (let i = 0; i < 20; i++) {
+    const heart = document.createElement('div');
+    heart.className = 'heart';
+    heart.innerHTML = '❤️';
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (3 + Math.random() * 5) + 's';
+    heart.style.fontSize = (16 + Math.random() * 20) + 'px';
+    document.body.appendChild(heart);
+}
+
+function toggleMusic() {
+    if (music.paused) {
+        music.play();
+        toggleBtn.innerHTML = '⏸ Pause';
+    } else {
+        music.pause();
+        toggleBtn.innerHTML = '▶️ Play';
+    }
+}
